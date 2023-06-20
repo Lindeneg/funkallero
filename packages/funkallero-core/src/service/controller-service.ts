376a63427ecf54d45f7bb3e519d20f24758708cd
lindeneg <christian@lindeneg.org>
2023-06-20 22:39:52 +0200

    make controller handleResult method public

--- packages/funkallero-core/src/service/controller-service.ts
+++ packages/funkallero-core/src/service/controller-service.ts
@@ -1,10 +1,11 @@
 import type { RouterOptions } from 'express';
 import type { HttpMethodUnion } from '../enums/http-method';
 import type IBaseService from './base-service';
+import type { MediatorResult } from './mediator-service';
 import type { Response } from '../types';
 import ScopedService from './scoped-service';
 
-export type ControllerFn = (...args: any[]) => Promise<void>;
+export type ControllerFn = (...args: any[]) => Promise<MediatorResult>;
 
 export interface IControllerSettings {
     authPolicy?: string | string[];
@@ -19,10 +20,14 @@ export interface IRoute {
     routerOptions: RouterOptions;
 }
 
-export interface IControllerService extends IBaseService {}
+export interface IControllerService extends IBaseService {
+    handleResult(result: MediatorResult<any>): Promise<void>;
+}
 
 abstract class ControllerService extends ScopedService implements IControllerService {
     protected readonly response: Response;
+
+    public abstract handleResult(result: MediatorResult<any>): Promise<void>;
 }
 
 export default ControllerService;
