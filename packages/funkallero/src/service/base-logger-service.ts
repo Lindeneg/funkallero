1f21c93c4e2075c8dad201d9d2c8b6dd19cb735a
lindeneg <christian@lindeneg.org>
2023-07-05 23:18:28 +0200

    make base logger palette configurable

--- packages/funkallero/src/service/base-logger-service.ts
+++ packages/funkallero/src/service/base-logger-service.ts
@@ -9,7 +9,50 @@ import {
     type ILoggerPayloadWithLevel,
     type LoggerPayload,
 } from '@lindeneg/funkallero-core';
-import LOG_LEVEL_COLOR from '../enums/log-level-color';
+
+export type BaseLoggerPaletteColor = string;
+export type BaseLoggerPaletteColorExtended = BaseLoggerPaletteColor | ((msg: string) => string);
+export type BaseLoggerPaletteObj = Record<LogLevelUnion, BaseLoggerPaletteColorExtended>;
+export type BaseLoggerPalette = Map<LogLevelUnion, BaseLoggerPaletteColorExtended>;
+
+const DISABLE_LOGGER = process.argv[2] === '--disable-funkallero-logger';
+
+const EMPTY_PALETTE = {
+    [LOG_LEVEL.INFO]: '',
+    [LOG_LEVEL.WARNING]: '',
+    [LOG_LEVEL.ERROR]: '',
+    [LOG_LEVEL.VERBOSE]: '',
+    [LOG_LEVEL.SILENT]: '',
+} as const;
+
+export class BaseLoggerServicePalette {
+    private static readonly palette: BaseLoggerPalette = new Map();
+
+    public static getColor(level: LogLevelUnion, msg = ''): BaseLoggerPaletteColor {
+        const match = BaseLoggerServicePalette.palette.get(level);
+
+        if (typeof match === 'function') {
+            return match(msg);
+        }
+
+        if (typeof match === 'string') {
+            return match + ' ' + msg;
+        }
+
+        return msg;
+    }
+
+    public static setPalette(palette: Partial<BaseLoggerPaletteObj>) {
+        const colors = Object.entries(palette);
+        for (const [level, color] of colors) {
+            BaseLoggerServicePalette.palette.set(Number.parseInt(level) as LogLevelUnion, color);
+        }
+    }
+
+    public static resetPalette() {
+        BaseLoggerServicePalette.setPalette(EMPTY_PALETTE);
+    }
+}
 
 class BaseLoggerService extends SingletonService implements ILoggerService {
     @injectService(SERVICE.CONFIGURATION)
@@ -32,6 +75,7 @@ class BaseLoggerService extends SingletonService implements ILoggerService {
     }
 
     private prepare(payload: LoggerPayload | string, level: LogLevelUnion) {
+        if (DISABLE_LOGGER) return;
         if (typeof payload === 'string') return this.log({ msg: payload, level });
         this.log({ ...payload, level });
     }
@@ -40,8 +84,8 @@ class BaseLoggerService extends SingletonService implements ILoggerService {
         if (this.configService.logLevel === LOG_LEVEL.SILENT || level === LOG_LEVEL.SILENT) return;
 
         if (force || this.configService.logLevel >= level) {
-            const color = LOG_LEVEL_COLOR[level];
-            const args: Array<string | Record<string, unknown>> = [color(msg)];
+            const coloredMsg = BaseLoggerServicePalette.getColor(level, msg);
+            const args: Array<string | Record<string, unknown>> = coloredMsg ? [coloredMsg] : [];
 
             if (this.configService.logLevel === LOG_LEVEL.VERBOSE && Object.keys(rest).length > 0) {
                 args.push({ ...rest });
