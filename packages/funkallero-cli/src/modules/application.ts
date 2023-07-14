7fc588d4a9d5247bcfb2dcdb11c1a8701b4a9233
lindeneg <christian@lindeneg.org>
2023-07-15 00:25:15 +0200

    add application module action

--- packages/funkallero-cli/src/modules/application.ts
+++ packages/funkallero-cli/src/modules/application.ts
@@ -1,4 +1,5 @@
-import { createModule, createTemplate } from '@lindeneg/scaffold-core';
+import { createModule, createTemplate, createAction, logger, joinPath } from '@lindeneg/scaffold-core';
+import { readFile, writeFile } from 'fs/promises';
 
 interface IndexFileTemplateData {
     exports: Array<{
@@ -15,7 +16,7 @@ const application = createModule({
     dependencies: [],
 
     templates: {
-        action: createTemplate<{ actionName: string; type: 'Command' | 'Query'; actionResult: string }, []>({
+        action: createTemplate<{ actionClassName: string; actionResult: string }, []>({
             data: {},
             templateFile: 'plop-templates/application/action.ts.hbs',
         }),
@@ -27,6 +28,36 @@ const application = createModule({
             data: {},
             templateFile: 'plop-templates/application/index.ts.hbs',
         }),
+    },
+
+    actions: {
+        addExportToApplicationIndex: createAction<{ rootDir: string; exportString: string }>(
+            'add-export-to-application-index',
+            async (_, { rootDir, exportString }) => {
+                try {
+                    const path = joinPath(rootDir, 'index.ts');
+                    const file = await readFile(path, { encoding: 'utf8' });
+
+                    const newContents = file.trimEnd() + '\n' + exportString;
+
+                    await writeFile(path, newContents, { encoding: 'utf8' });
+
+                    return 'successfully added application export';
+                } catch (err) {
+                    const msg = 'failed to add application export';
+
+                    logger.error({
+                        source: 'modules.application.actions.addExportToApplicationIndex',
+                        msg,
+                        err,
+                        rootDir,
+                        exportString,
+                    });
+
+                    throw msg;
+                }
+            }
+        ),
     },
 });
 
