3c42e2ce6d364a4dbfcb1377a241b5506c0adc54
lindeneg <christian@lindeneg.org>
2023-07-15 00:25:02 +0200

    export generators

--- packages/funkallero-cli/src/generators/index.ts
+++ packages/funkallero-cli/src/generators/index.ts
@@ -1,1 +1,4 @@
 export { default as initGenerator } from './init';
+export { default as controllerGenerator } from './controller';
+export { default as commandGenerator } from './action/command';
+export { default as queryGenerator } from './action/query';
