3c4a86811aaac9989819c9fcc03334b54a3c4ad8
lindeneg <christian@lindeneg.org>
2023-07-15 12:40:09 +0200

    disable ts cache

--- packages/funkallero-cli/rollup.config.js
+++ packages/funkallero-cli/rollup.config.js
@@ -20,6 +20,7 @@ export default () => {
             commonjs(),
             typescript({
                 tsconfig: './tsconfig.json',
+                clean: true,
             }),
         ],
     };
