a67ac594d610682d648969beac799ddda4be0b5a
lindeneg <christian@lindeneg.org>
2023-07-21 23:40:24 +0200

    fix typo

--- locust/main.py
+++ locust/main.py
@@ -12,7 +12,7 @@ def _(environment, **kw):
         logging.error("Test failed due to average response time ratio > 250 ms")
         environment.process_exit_code = 1
     elif environment.stats.total.get_response_time_percentile(0.95) > 800:
-        logging.error("Test failed due to 95th percentile response time > 600 ms")
+        logging.error("Test failed due to 95th percentile response time > 800 ms")
         environment.process_exit_code = 1
     else:
         environment.process_exit_code = 0
