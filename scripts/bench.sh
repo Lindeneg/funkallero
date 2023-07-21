21f68ac35bbed7384111c228af0490d06407502d
lindeneg <christian@lindeneg.org>
2023-07-22 00:04:14 +0200

    lower load

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -2,7 +2,7 @@
 
 LATEST="./locust/report/latest.json"
 
-CMD="python3 -m locust --headless --json -f ./locust/main.py --users 20 --spawn-rate 5 --run-time 1m --stop-timeout 10s -H http://localhost:3000/api"
+CMD="python3 -m locust --headless --json -f ./locust/main.py --users 10 --spawn-rate 2 --run-time 30s --stop-timeout 5s -H http://localhost:3000/api"
 
 $CMD > $LATEST
 
