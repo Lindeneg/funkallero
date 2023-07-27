c84bd7bcec73fd4b8df34fba9eb2811b4334841a
lindeneg <christian@lindeneg.org>
2023-07-28 01:01:36 +0200

    update locus

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -7,12 +7,6 @@ def _(environment, **kw):
     if environment.stats.total.fail_ratio > 0.01:
         logging.error("Test failed due to failure ratio > 1%")
         environment.process_exit_code = 1
-    elif environment.stats.total.avg_response_time > 250:
-        logging.error("Test failed due to average response time ratio > 250 ms")
-        environment.process_exit_code = 1
-    elif environment.stats.total.get_response_time_percentile(0.95) > 800:
-        logging.error("Test failed due to 95th percentile response time > 800 ms")
-        environment.process_exit_code = 1
     else:
         environment.process_exit_code = 0
 
