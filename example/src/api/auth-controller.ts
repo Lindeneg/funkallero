2716bbb110d085cc09be3ad9ac7e12f7a5f6e54e
lindeneg <christian@lindeneg.org>
2023-07-29 14:39:51 +0200

    play around with versioning

--- example/src/api/auth-controller.ts
+++ example/src/api/auth-controller.ts
@@ -3,12 +3,29 @@ import Controller from './controller';
 import SERVICE from '@/enums/service';
 import loginSchema from '@/dtos/login-dto';
 import signupSchema from '@/dtos/signup-dto';
+import { MediatorResult, Promisify, Request } from '@lindeneg/funkallero-core';
+
+interface IVersioningService {
+    extractVersionFromRequest(request: Request): Promisify<string>;
+    resolveRequestHandlerFromVersion(version: string): Promisify<(...args: unknown[]) => Promisify<MediatorResult>>;
+}
+
+const version = controller;
 
 @controller()
+@version('1')
 class AuthController extends Controller {
+    @version('1')
     @httpPost('/login')
     @after(SERVICE.COOKIE_MIDDLEWARE)
-    public async login(@body(loginSchema) loginDto: ParsedSchema<typeof loginSchema>) {
+    public async loginV2(@body(loginSchema) loginDto: ParsedSchema<typeof loginSchema>) {
+        return this.mediator.send('LoginCommand', loginDto);
+    }
+
+    @version('2')
+    @httpPost('/login')
+    @after(SERVICE.COOKIE_MIDDLEWARE)
+    public async loginV1(@body(loginSchema) loginDto: ParsedSchema<typeof loginSchema>) {
         return this.mediator.send('LoginCommand', loginDto);
     }
 
