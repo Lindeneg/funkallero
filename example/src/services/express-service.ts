8413f779e430ade48d9f18376e27ad792c845514
lindeneg <christian@lindeneg.org>
2023-08-11 04:00:08 +0200

    allow html response on unregistered routes

--- example/src/services/express-service.ts
+++ example/src/services/express-service.ts
@@ -1,6 +1,6 @@
 import { static as expressStatic } from 'express';
 import cookieParser from 'cookie-parser';
-import { HttpException, BaseExpressService } from '@lindeneg/funkallero';
+import { BaseExpressService } from '@lindeneg/funkallero';
 
 class ExpressService extends BaseExpressService {
     public async setup() {
@@ -8,15 +8,6 @@ class ExpressService extends BaseExpressService {
 
         this.app.use(cookieParser());
         this.app.use(expressStatic('public'));
-    }
-
-    public async onLastRouteAdded() {
-        this.app.use((req, res, next) => {
-            if (!req.path.startsWith('/api')) {
-                (req as any)._funkallero = { html: true };
-            }
-            next(HttpException.notFound());
-        });
     }
 }
 
--- packages/funkallero/src/service/base-error-handler-service.ts
+++ packages/funkallero/src/service/base-error-handler-service.ts
@@ -3,6 +3,7 @@ import {
     SingletonService,
     HttpException,
     injectService,
+    type Response,
     type IExpressErrorhandlerService,
     type ExpressErrorHandlerFnArgs,
     type ILoggerService,
@@ -26,18 +27,27 @@ class BaseRequestErrorHandlerService extends SingletonService implements IExpres
             error: err,
         });
 
-        if (isHtmlResponse) {
-            this.logger.info('sending html error response on ' + req.id);
-            return res.status(errCode).send(`<h1>${errCode} Something went wrong. Please try again.</h1>`);
+        if (isHtmlResponse) return this.handleHtmlResponse(err, res, errCode);
+
+        return this.handleJsonResponse(err, res, errCode);
+    }
+
+    private async handleHtmlResponse(err: any, res: Response, errCode: number) {
+        if (err instanceof HttpException) {
+            return res.status(errCode).send(await err.toHtml());
         }
 
+        return res.status(errCode).send(`<h1>${errCode} Something went wrong..</h1>`);
+    }
+
+    private handleJsonResponse(err: any, res: Response, errCode: number) {
         if (err instanceof HttpException) {
-            res.status(errCode).json(err.toResponse());
-        } else {
-            res.status(errCode).json({
-                message: 'Something went wrong. Please try again.',
-            });
+            return res.status(errCode).json(err.toResponse());
         }
+
+        return res.status(errCode).json({
+            message: 'Something went wrong. Please try again.',
+        });
     }
 }
 
