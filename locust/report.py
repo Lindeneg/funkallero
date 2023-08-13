031190d3c5ffb3f13c7c346a9376687a6c190ea4
lindeneg <christian@lindeneg.org>
2023-08-13 23:14:22 +0200

    update thresholds

--- locust/report.py
+++ locust/report.py
@@ -8,10 +8,12 @@ ReportComparisonResult = tuple[str, ReportTestError]
 base_path = os.path.join(os.sep.join(__file__.split(os.sep)[:-1]), "report")
 is_express = sys.argv[1] == "express"
 
+base_increase_percent = 25 if is_express else 10
+
 min_accumulated_request_percent = 80 if is_express else 50
-average_percent_max_increase = 15 if is_express else 5
-max_percent_max_increase = 20 if is_express else 10
-min_percent_max_increase = 20 if is_express else 10
+average_percent_max_increase = base_increase_percent
+max_percent_max_increase = base_increase_percent
+min_percent_max_increase = base_increase_percent
 
 
 green_check_mark = "\U00002705"
